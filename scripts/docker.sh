f59d3f382fba3e246016f12e99b06fcd1ea0e020
Damien DUPORTAL <damien.duportal@gmail.com>
2017-02-17 21:30:43 +0100

    Alpine Host configuration for cgroup swap accounting + grsec allowing soft pax
    Signed-off-by: Damien DUPORTAL <damien.duportal@gmail.com>

--- scripts/docker.sh
+++ scripts/docker.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 set -eux
 
-# Install Docker
+#### Configure System to handle Docker capabilities
+
+# Make PAX in softmode - example for container with JVM
+cat <<EOF >/etc/sysctl.d/10-docker.conf
+kernel.pax.softmode=1
+EOF
+
+# Enable swap accounting for containers
+sed -i 's/quiet/quiet cgroup_enable=memory swapaccount=1/' /boot/extlinux.conf
+
+
+### Install Docker
 echo 'http://dl-cdn.alpinelinux.org/alpine/edge/community' \
     | tee -a /etc/apk/repositories
 apk update
@@ -14,10 +25,10 @@ addgroup "${BASE_USER}" docker
 service docker start
 rc-update add docker boot
 
-# Install Docker-compose
+### Install Docker-compose
 apk add py-pip
 pip install --no-cache-dir --upgrade pip
 pip install --no-cache-dir "docker-compose==${COMPOSE_VERSION}"
 
-# Docker bash completions
+### Docker bash completions
 apk add docker-bash-completion
